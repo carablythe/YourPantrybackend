5459c895e69ab0f4e8b29c5debe1c88e8db67286
Cara Phillips <caraphillips@Caras-MacBook-Pro.local>
2022-11-15 18:42:46 +0900

    started new app with basic dependencies

--- README.md
+++
@@ -1,2 +0,0 @@
-# Please read the ReadMe.md file for the frontend of this app for more information: https://github.com/carablythe/YourPantryfrontend/blob/master/README.md
-
--- server.js
+++ server.js
@@ -1,66 +1,13 @@
-//___________________
-//Dependencies
-//___________________
+// server.js
+
 const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const cors = require('cors')
-const app = express ();
-const db = mongoose.connection;
-require('dotenv').config()
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3003;
 
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI  = process.env.MONGODB_URI;
+const app = express();
 
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true}
-);
+app.get('/', (req, res) => res.send('Hello world!'));
 
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
+const port = process.env.PORT || 8082;
 
-//___________________
-//Middleware
-//___________________
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-//use public folder for static assets
-app.use(express.static('public'));
 
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
-app.get("/", (req,res) => {
-  res.send("hello")
-})
-
-app.use(cors())
-
-mongoose.connection.once('open', () => {
-  console.log('connected to mongod...');
-});
-//___________________
-// Routes
-//___________________
-//localhost:3000
-const recipeController = require ('./controllers/yourpantry.js');
-app.use('/yourpantry', recipeController)
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
